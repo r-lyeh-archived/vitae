5e9d60980d46ef4b4629eb62045050d3b05e5cfa
r-lyeh <rlyeh.nospam@gmail.com>
2014-08-16 10:53:42 +0200

    Updated

--- main.cpp
+++ main.cpp
@@ -1,28 +1,27 @@
-/** A condensed resume - https://r-lyeh.github.io  *
- * - rlyeh                                        **/
+/** A condensed resume - https://github.com/r-lyeh/status  *
+ * - rlyeh                                                **/
 
 #ifdef RESUME_EXTRA
 #   include <stdlib.h>                      // C
 #   include <algorithm>                     // C++ & STL
 #   include <thread>                        // C++11
 
-#   include <v8/v8.h>                       // V8, Javascript
-#   include <lua/lua.h>                     // Lua, Moonscript
-#   include <angelscript/angelscript.h>     // Angelscript
-#   include <squirrel/squirrel.h>           // Squirrel
-#   include <lisp/many.h>                   // CL, derivatives
-#   include <scheme/many.h>                 // R6RS, derivatives
+#   include <scripting>                     // V8+Javascript, Lua+Moonscript, runtime C/C++,
+                                            // Angelscript, Squirrel, Lisp's, Scheme's...
+
+#   include <memory>                        // Allocators, Stack allocators, Arenas...
+#   include <compressors>                   // Huffman, Deflate, LZMA, CM...
+#   include <messaging>                     // XML, JSON, MSGPack, C++ serialization...
 
 #   include <GL/gl.h>                       // OpenGL, shaders, extensions, glut...
 #   include <AL/al.h>                       // OpenAL
+#   include <ui>                            // UI/UX, easings, interpolators...
 
 #   include <Windows.h>                     // Win32 API
 #   include <unistd.h>                      // Unix & Posix APIs
 
 #   include <sys/socket.h>                  // TCP, UDP sockets
 #   include <enet/enet.h>                   // Reliable UDP sockets, NAT traversal
-
-#   include <msgpack.hpp>                   // Serialization, messaging
 //  [...]                                   // Etc.
 #endif
 
@@ -30,13 +29,13 @@
 #include <string>
 #include <map>
 
-int main() {
-    struct string : public std::string {
-        string() {}
-        std::string operator =( const std::string &s ) {
-            return assign( s.find(*this) != npos ? s : *this + "\n\t" + s );
-    }   };
+struct string : public std::string {
+    string() {}
+    std::string operator =( const std::string &s ) {
+        return assign( s.find(*this) != npos ? s : *this + "\n\t" + s );
+} };
 
+int main() {
     std::map< int, string > all, &bio = all, &edu = all,
         &task = all, &game = all, &like = all, &meta = all, &link = all;
 
@@ -50,14 +49,16 @@ int main() {
     task[ 2009 ] = "Lead programmer, master submission @ VirtualToys/Barcelona/ES";
     task[ 2010 ] =
     task[ 2011 ] = "Lead server programmer, dev tools, networking @ VirtualToys/Barcelona/ES";
+    task[ 2013 ] =
+    task[ 2014 ] = "Lord of Memory and Compression Wizard @ Gameloft/Madrid/ES";
 
     game[ 2007 ] = "Imagine: Pet Vet (DS)";
     game[ 2008 ] = "My Eco Planet (DS & DSi)";
     game[ 2008 ] = "Yummy Yummy Cooking Jam (DS)";
     game[ 2008 ] = "Rafa Nadal Tennis (DS)";
     game[ 2009 ] = "Cooking Party (Wii)";
-    game[ 2009 ] = "Spaceballs (PS3)(port)(cancelled)";
-    game[ 2009 ] = "Yummy Yummy Cooking Jam (PS3)(port)(cancelled)";
+    game[ 2009 ] = "Spaceballs (PS3)(port)(canceled)";
+    game[ 2009 ] = "Yummy Yummy Cooking Jam (PS3)(port)(canceled)";
     game[ 2010 ] =
     game[ 2011 ] = "Freak Wars: Torrente Online 2 (Windows)";
 
@@ -72,10 +73,11 @@ int main() {
 
     meta[ 2006 ] = "<3 Pixels";
     meta[ 2006 ] = "<3 Zombies";
-    meta[ 2010 ] = "<3 Rock";
+    meta[ 2010 ] = "<3 Post Rock";
     meta[ 2010 ] = "<3 Scifi";
+    meta[ 2013 ] = "<3 Stoner Rock";
 
-    link[ 2014 ] = "https://r-lyeh.github.io/";
+    link[ 2014 ] = "https://github.com/r-lyeh/status";
 
     for( auto &entry : all )
         std::cout << entry.first << ":\t" << entry.second << std::endl;
